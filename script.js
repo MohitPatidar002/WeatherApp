90c14fafb4cd86c0417faaa675807f3f01d598fc
Mohit Patidar <125687678+MohitPatidar002@users.noreply.github.com>
2023-12-02 19:16:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let inputvalue = document.querySelector('#searchCity');
 let cityName = document.querySelector('#cityName');  
 let countryflag = document.querySelector('#countryFlag');
 let loadingImg = document.querySelector('#loadingImg');
+let cloudPercent = document.querySelector('#cloudPercent');
+let weatherData = document.querySelector('.weatherData');
 let api = '3a88fa84b7d2ded2999d4212a3b2a9e8';
 
 
@@ -28,37 +30,30 @@ submitForm.addEventListener('submit', (e) => {
 
 
 async function showWeatherData(city){
-    loadingImg.classList.add('active');
-    cityName.classList.remove('active');
-    countryflag.classList.remove('active');
-    cloudImg.classList.remove('active');
-    box.classList.remove('active');
-    TempDisplay .classList.remove('active');
 
+    loadingImg.classList.add('active');
+    weatherData.classList.remove('active');
 
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`);
     let data = await response.json();
     console.log(data);
     
     loadingImg.classList.remove('active');
-    TempDisplay .classList.add('active');
-    cityName.classList.add('active');
-    countryflag.classList.add('active');
-    cloudImg.classList.add('active');
-    box.classList.add('active');
+    weatherData.classList.add('active');
 
     PutWeatherData(data);
 }
 
 function PutWeatherData(data){
-    cityName.innerText = `${data?.name.toUpperCase()}`;
+   cityName.innerText = `${data?.name.toUpperCase()}`;
    countryflag.src=`https://flagcdn.com/144x108/${data?.sys?.country.toLowerCase()}.png`;
 
-   TempDisplay.innerText = `${data.main.temp.toFixed(2)} °C`;
+   TempDisplay.innerText = `${data?.main?.temp.toFixed(2)} °C`;
    cloudImg.src=`https://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`;
 
-   windspeed.innerText = `${data.wind.speed.toFixed(2)}m/s`;
-   CurrHumidity.innerText = `${data.main.humidity}%`;
+   windspeed.innerText = `${data?.wind?.speed.toFixed(2)}m/s`;
+   CurrHumidity.innerText = `${data?.main?.humidity}%`;
    CloudDisplay.innerText = `${data?.weather?.[0]?.main}`;
+   cloudPercent.innerText = `${data?.clouds?.all}%`;
 }
 
